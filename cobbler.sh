0437662e202937f6b4ece305e803ff74a201b75e
dujincan <jincan.du@outlook.com>
2018-11-27 18:23:50 +0800

    modify

--- cobbler.sh
+++ cobbler.sh
@@ -7,24 +7,17 @@ curl -o /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
 # 安装所需软件
 yum -y install cobbler cobbler-web dhcp tftp-server pykickstart httpd python-django
 
-#设置相关软件开机自启动并启动
+#设置相关软件开机自启动
 systemctl enable cobblerd httpd tftp.socket rsyncd
-systemctl start cobblerd httpd tftp.socket rsyncd
-systemctl is-active cobblerd httpd tftp.socket rsyncd
-
-# 修改tftp相关配置
+systemctl start httpd tftp.socket rsyncd cobblerd
 
 
-# 下载boot-loaders
-/usr/bin/cobbler get-loaders
-
-# 修改cobbler配置文件
-sed -i '14s#yes#no#' /etc/xinetd.d/tftp
-
-
-# 防止误重装
+# 修改cobbler相关文件
 subnet=172.16.1
 cobbler_ip=$(/usr/sbin/ip addr show eth1 |awk -F "[ /]*" 'NR==3{print $3}')
+# 修改tftp相关配置
+sed -i '14s#yes#no#' /etc/xinetd.d/tftp
+# 防止误重装
 sed -i "s#pxe_just_once: 0#pxe_just_once: 1#" /etc/cobbler/settings 
 # 配置Cobbler统一管理DHCP
 sed -i "s#manage_dhcp: 0#manage_dhcp: 1#" /etc/cobbler/settings
@@ -35,13 +28,23 @@ sed -i "s#server: 127.0.0.1#server: $cobbler_ip#;s#next_server: 127.0.0.1#next_s
 # 修改系统的密码为123456 可以根据需求使用openssl passwd -1 生成新的加密密码
 sed -i 's#$1$mF86/UHC$WvcIcX2t6crBz2onWxyac.#$1$gAR8NnAS$7B21qYq0J2oQt06wdlT5T.#' /etc/cobbler/settings
 
-
-# 重启cobbler
-systemctl restart httpd
+# 重启cobbler 
 systemctl restart cobblerd
+sleep 2
+
+# 同步cobbler配置
+/usr/bin/cobbler sync
+
+# 下载boot-loaders
+/usr/bin/cobbler get-loaders
+
+# 再次重启cobbler
+systemctl restart cobblerd
+sleep 2
 /usr/bin/cobbler sync
 
 # 访问信息
+echo ""
 ext_ip=$(/usr/sbin/ip addr show eth0 |awk -F "[ /]*" 'NR==3{print $3}')
 echo -e "######## Cobbler installed successfully ########\nplease visit https://$ext_ip/cobbler_web\nuser=cobbler password=cobbler"
 
